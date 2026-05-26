e2431c8c3048b166e7041560a74e0b2b8a6be609
EEtinkerer <151823115+EEtinkerer@users.noreply.github.com>
2026-05-26 14:41:17 -0700

    Delete files/user/lib/lua/luci/netwatch.lua

--- files/user/lib/lua/luci/netwatch.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.netwatch", package.seeall)
-
-function index()
-    entry({"admin", "system", "netwatch"}, cbi("netwatch"), _("NetWatch"), 90).dependent = true
-    entry({"admin", "system", "netwatch", "logs"}, template("netwatch/logs"), _("NetWatch Logs"), 91).dependent = true
-end
